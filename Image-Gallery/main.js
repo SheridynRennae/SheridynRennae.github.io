bdbb713ff99cfdeb8e47b0dc2eff0081a9522677
SheridynRennae <sheridyn.sharpe@dcmail.ca>
2025-08-01 00:58:57 -0400

    Assignment 4: Gallery Commit 4
    Part 2: Declared the alt text for each image file and also created a for of loop to set the src and alt attributes to each image and add them to the thumb bar.

--- Image-Gallery/main.js
+++ Image-Gallery/main.js
@@ -21,13 +21,28 @@ const images = [
 ]
 
 /* Declaring the alternative text for each image file */
-
+const altText = {
+    "pic1.jpg": "A close up shot of a human eye.",
+    "pic2.jpg": "A pastel painting of a wave of the sea.",
+    "pic3.jpg": "A collection of white and purple flowers.",
+    "pic4.jpg": "An ancient Egyptian cave marking.",
+    "pic5.jpg": "A brown moth resting on a leaf."    
+}
 
 /* Looping through images */
+for (const image of images) {
+    const newImage = document.createElement('img');
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    // Set the src and alt attribute of the img element using template literal (for src concat.) and array look-up.
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', altText[image]);
+
+    // Adds new img to the thumb bar.
+    thumbBar.appendChild(newImage);
+
+
+}
+
+
 
 /* Wiring up the Darken/Lighten button */
