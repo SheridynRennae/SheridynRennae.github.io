4d9b8cdaa1ec7301984f86be469077cf0cb073ae
SheridynRennae <sheridyn.sharpe@dcmail.ca>
2025-08-01 15:07:31 -0400

    Assignment 4: Balls Commit 4
    Part 4: Began initializing the EvilCircle class. The class extends Shape, is only passed x and y args that are passed up to the Shape superclass along with velX and velY values of 20.
    The colour and size were set, and the provided event listener was added to the constructor.
    Finally, the draw method was implemented, using the same values as the one in Ball, with changes to stroke/strokeStyle and lineWidth.

--- Bouncing-Balls-Features/main-finished.js
+++ Bouncing-Balls-Features/main-finished.js
@@ -72,7 +72,7 @@ class Ball extends Shape {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -82,6 +82,38 @@ class Ball extends Shape {
         }
       }
     }
+  }
+}
+
+class EvilCircle extends Shape {
+    constructor(x, y) {
+        super(x, y, 20, 20)
+        this.color = "white";
+        this.size = 10;
+
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "a":
+                    this.x -= this.velX;
+                    break;
+                case "d":
+                    this.x += this.velX;
+                    break;
+                case "w":
+                    this.y -= this.velY;
+                    break;
+                case "s":
+                    this.y += this.velY;
+                    break;
+            }
+        });
+    }
+    draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
   }
 }
 
